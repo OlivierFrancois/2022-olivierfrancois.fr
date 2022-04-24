ddb21d5d5a3c722fd913b0b0d76c4a8348ac5ea2
ofrancois <o.francois@adoxia.fr>
2022-04-24 15:44:25 +0200

    En cours

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,32 +7,18 @@ module.exports = {
     theme: {
         extend: {
             colors : {
-                mariboo:{
-                    primary: '#ffbf14',
-                    dark: '#e2a90c',
-                    darker: '#bf8302',
-                },
-                livounet:{
-                    primary: '#0f718c',
-                    dark: '#0c5367',
-                    darker: '#083a48',
-                },
-                theme:{
-                    light: '#EDEFF5',
-                    dark: '#25262D',
-                },
-                header: '#333344',
-                light: '#f1f1f1',
-            },
-            height: {
-                header: '5rem',
-                headermin: '45px',
+                primary: '#08afab',
+                primarylight: '#26ccb0',
+                secondary: '#92268e',
+                dark: '#2a2a2a',
+                lightgrey: '#bfbfbf',
+                smoothwhite: '#f1f1f1',
             },
             transitionProperty: {
                 fweight: "font-weight"
             },
             fontFamily: {
-                logo: ["Gill Sans MT"]
+                acme: ["Acme"]
             }
         }
     },
